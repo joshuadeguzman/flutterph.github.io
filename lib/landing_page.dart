65a2dbd3746146511269d4f5efa422e472739426
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-12-01 12:59:18 +0800

    Initial commit

---
+++ lib/landing_page.dart
@@ -0,0 +1,23 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LandingPageState();
+  }
+}
+
+class LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
